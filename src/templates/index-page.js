29deaa3279538bf6e08eb1a286793bf3f9f0edfb
Daniel Lanthier <dlanthier@circleci.com>
2020-06-19 11:02:55 -0400

    Removed the blurbs

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -86,21 +86,13 @@ export const IndexPageTemplate = ({
                     <p>{description}</p>
                   </div>
                 </div>
-                <Features gridItems={intro.blurbs} />
-                <div className="columns">
-                  <div className="column is-12 has-text-centered">
-                    <Link className="btn" to="/products">
-                      See all products
-                    </Link>
-                  </div>
-                </div>
                 <div className="column is-12">
                   <h3 className="has-text-weight-semibold is-size-2">
                     Latest Events
                   </h3>
                   <BlogRoll />
                   <div className="column is-12 has-text-centered">
-                    <Link className="btn" to="/blog">
+                    <Link className="btn" to="/events">
                       Read more
                     </Link>
                   </div>
