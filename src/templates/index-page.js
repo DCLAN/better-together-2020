b04685f21bccdd2d865305ac19c922eb156fe472
Daniel Lanthier <dlanthier@circleci.com>
2020-07-09 19:30:23 -0400

    Removing blurbs from GraphQL calls

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -167,16 +167,6 @@ export const pageQuery = graphql`
         }
         description
         intro {
-          blurbs {
-            image {
-              childImageSharp {
-                fluid(maxWidth: 240, quality: 64) {
-                  ...GatsbyImageSharpFluid
-                }
-              }
-            }
-            text
-          }
           heading
           description
         }
